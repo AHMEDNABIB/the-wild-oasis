c7fe1570b2408c4daed878fd42107963a52cce2a
AHMED NABIB <nabibpallab@gmail.com>
2023-07-11 17:04:51 +0600

    form validation and image upload complete

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://lblbiignvyuopljnuwzj.supabase.co";
+export const supabaseUrl = "https://lblbiignvyuopljnuwzj.supabase.co";
 const supabaseKey =
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxibGJpaWdudnl1b3Bsam51d3pqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg5MTI5MzYsImV4cCI6MjAwNDQ4ODkzNn0.tWyL5-PImdxo7PVgn0zjF30oTIDewNvSqm64HKX3Sw0";
 const supabase = createClient(supabaseUrl, supabaseKey);
