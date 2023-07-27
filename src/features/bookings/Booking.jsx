aed7cba6e7d6025831f0c8289299d633df76d8a1
AHMED NABIB <nabibpallab@gmail.com>
2023-07-28 00:56:20 +0600

    single booking complete

---
+++ src/features/bookings/Booking.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import BookingDetail from './BookingDetail';
+
+const Booking = () => {
+  return (
+   <BookingDetail/>
+  )
+}
+
+export default Booking
--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -10,6 +10,9 @@ import ButtonText from "../../ui/ButtonText";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
 
+import Spinner from "../../ui/Spinner";
+import { useBooking } from './useBooking';
+
 const HeadingGroup = styled.div`
   display: flex;
   gap: 2.4rem;
@@ -17,10 +20,21 @@ const HeadingGroup = styled.div`
 `;
 
 function BookingDetail() {
-  const booking = {};
-  const status = "checked-in";
+  // const booking = {};
+
+  const { booking, isLoading } = useBooking();
+  
+  const{id:bookingId,status}= booking
+
+  
+  
+
+
+
 
   const moveBack = useMoveBack();
+
+  if(isLoading) return <Spinner/>
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -29,23 +43,25 @@ function BookingDetail() {
   };
 
   return (
-    <>
-      <Row type="horizontal">
-        <HeadingGroup>
-          <Heading as="h1">Booking #X</Heading>
-          <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
-        </HeadingGroup>
-        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
-      </Row>
+		<>
+			<Row type="horizontal">
+				<HeadingGroup>
+          <Heading as="h1">Booking #{ bookingId }</Heading>
+					<Tag type={statusToTagName[status]}>
+						{status.replace("-", " ")}
+					</Tag>
+				</HeadingGroup>
+				<ButtonText onClick={moveBack}>&larr; Back</ButtonText>
+			</Row>
 
-      <BookingDataBox booking={booking} />
+			<BookingDataBox booking={booking} />
 
-      <ButtonGroup>
-        <Button variation="secondary" onClick={moveBack}>
-          Back
-        </Button>
-      </ButtonGroup>
-    </>
+			<ButtonGroup>
+				<Button variation="secondary" onClick={moveBack}>
+					Back
+				</Button>
+			</ButtonGroup>
+		</>
   );
 }
 
