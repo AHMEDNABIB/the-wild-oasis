ffc070af3cf2d59c51ab4d3d883144ef0b039d9a
AHMED NABIB <nabibpallab@gmail.com>
2023-07-29 02:21:06 +0600

    refactor

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -3,19 +3,15 @@ import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
-
 export function useLogin() {
-    const queryClient = useQueryClient()
-    const navigate = useNavigate();
-    const { mutate: login, isLoading } = useMutation({
-		mutationFn: ({ email, password }) =>
-			loginApi({
-				email,
-				password,
-			}),
-        onSuccess: (user) => {
-            queryClient.setQueriesData(['user'],user)
-			navigate("/dashboard");
+	const queryClient = useQueryClient();
+	const navigate = useNavigate();
+
+	const { mutate: login, isLoading } = useMutation({
+		mutationFn: ({ email, password }) => loginApi({ email, password }),
+		onSuccess: (user) => {
+			queryClient.setQueryData(["user"], user.user);
+			navigate("/dashboard", { replace: true });
 		},
 		onError: (err) => {
 			console.log("ERROR", err);
@@ -23,8 +19,5 @@ export function useLogin() {
 		},
 	});
 
-    return {login,isLoading}
-
+	return { login, isLoading };
 }
-        
-    
